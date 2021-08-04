a28c1c9a79c1c28078260808b4635f41627404a4
faqar <fariba.qarib@cortado.com>
2021-08-04 12:29:37 +0200

    create house-Interface

---
+++ src/app/models/house.ts
@@ -0,0 +1,21 @@
+export interface IHouse {
+  url: string;
+  name: string;
+  region: string;
+  coatOfArms: string;
+  words: string;
+  titles: string[];
+  seats: string[];
+  currentLord: string;
+  heir: string;
+  overlord: string;
+  founded: string;
+  founder: string;
+  diedOut: string;
+  ancestralWeapons: string[];
+  cadetBranches: [];
+  swornMembers: [];
+}
+export interface IHouseList {
+  housesList: IHouse[];
+}
