f50fb74913fbba7da11c96de47474492bcdfd706
faqar <fariba.qarib@cortado.com>
2021-08-08 17:37:40 +0200

    Move get-character-http-Request from character.service to data.service

--- src/app/services/character.service.ts
+++ src/app/services/character.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ICharacter } from '../models/character';
-import configuration from 'src/configuration.json';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  constructor(private http: HttpClient) {}
+  constructor(private dataService: DataService<any>) {}
 
   getCharacter(url: string): Observable<ICharacter> {
-    return this.http.get<any>(url);
+    return this.dataService.getRaw$(url);
   }
 }
--- src/app/services/house.service.ts
+++ src/app/services/house.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-import configuration from 'src/configuration.json';
 import { IHouse } from '../models/house';
 import { DataService } from './data.service';
 import { tap } from 'rxjs/operators';
@@ -25,15 +23,12 @@ export class HouseService {
         list?.items.forEach((house) => this.assignId(house));
       })
     );
-    //return this.http.get<any>(this.apiUrl + '/houses', { observe: 'response', params: params });
   }
   getSpecificHouse(url: string): Observable<IHouse> {
     return this.dataService.getRaw$(url);
-    //return this.http.get<IHouse>(url);
   }
   getHouseWithId(id: string): Observable<any> {
     return this.dataService.getSingle$(RESOURCE_NAME, id).pipe(tap((house: IHouse) => this.assignId(house)));
-    // return this.http.get<IHouse>(this.apiUrl + '/houses/' + id);
   }
 
   private assignId(house: IHouse) {
